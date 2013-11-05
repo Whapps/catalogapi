ba028497c49d6f90bd1422e4da01e29ca4e9d1ef
Marc Slagle <marc.slagle@online-rewards.com>
2013-11-05 12:22:34 -0500

    headers and external id prefixes

--- perl/Whapps-CatalogAPI/lib/Whapps/CatalogAPI.pm
+++ perl/Whapps-CatalogAPI/lib/Whapps/CatalogAPI.pm
@@ -705,7 +705,12 @@ sub _make_get_request
     my ($self, %args) = @_;
     
     $self->_reset_errors();
-    
+   
+    if (($self->{external_id_prefix}) && ($args{external_user_id}))
+    {
+        $args{external_user_id} = $self->{external_id_prefix} . $args{external_user_id}
+    }
+ 
     my $creds = $self->_generate_creds( method => $args{method} );
     my $method = delete $args{method};
     
@@ -715,8 +720,22 @@ sub _make_get_request
     @args{keys %$creds} = values %$creds;
     my @params = map { $_ . '=' . uri_escape_utf8($args{$_}) } keys %args;
     my $uri = "https://$self->{endpoint}/$API_VERSION/rest/$method/?" . join('&', @params);
-    
-    my $response = $AGENT->get($uri);
+    my %meta;
+    if ($self->{meta})
+    {
+        while (my($k,$v) = each %{$self->{meta}})
+        {
+            if ($k =~ /^x-meta/)
+            {
+                $meta{$k} = $v;
+            }
+            else
+            {
+                $meta{'x-meta-' . $k} = $v;
+            }
+        }
+    }
+    my $response = $AGENT->get($uri, %meta);
     my $response_ref;
     eval {
         $response_ref = from_json($response->content);
@@ -754,7 +773,6 @@ sub _create_order
     my ($self, $order_ref) = @_;
     
     $self->_reset_errors();
-    
     my $method = 'order_place';
     
     my $creds = $self->_generate_creds( method => $method );
