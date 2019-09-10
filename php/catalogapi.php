2f730851bb4f2ce740680b4720557b207d1afa00
Dennis Smith <dennis.smith@online-rewards.com>
2019-09-10 16:35:16 -0400

    Added Cart Methods

--- php/catalogapi.php
+++ php/catalogapi.php
@@ -6,101 +6,435 @@ class CatalogAPI
 {
     // subdomain: the first part of your SUBDOMAIN.dev.catalogapi.com domain
     // secret key: the secret key you were given
-    // endpoint: use "rest" for JSON responses or "restx" for XML responses
     // is_prod, FALSE for dev, TRUE for production (you must you your prod key if this is TRUE)
-    function __construct($sub_domain, $secret_key, $endpoint="rest", $is_prod=FALSE)
+    function __construct($sub_domain, $secret_key, $is_prod = FALSE)
     {
         $this->sub_domain = $sub_domain;
         $this->secret_key = $secret_key;
-        $this->endpoint = $endpoint; # rest or restx
         $this->is_prod = $is_prod;
         $this->error = NULL;
-    }
-    
-    function list_available_catalogs()
-    {
-        return $this->_make_request("list_available_catalogs", array());
-    }
-    
-    function catalog_breakdown($socket_id, $is_flat=FALSE)
-    {
-        return $this->_make_request("catalog_breakdown", array("socket_id" => $socket_id, "is_flat" => ($is_flat?"1":"0")));
-    }
-    
-    function search_catalog($socket_id, $search_args=array())
-    {
-        $search_args["socket_id"] = $socket_id;
-        return $this->_make_request("search_catalog", $search_args);
-    }
-    
-    function view_item($socket_id, $catalog_item_id)
-    {
-        return $this->_make_request("view_item", array("socket_id" => $socket_id, "catalog_item_id" => $catalog_item_id));
+        $this->server_error = "";
+        $this->client_error = "";
     }
 
-    function _make_request($method, $args)
+
+
+    /*
+        Function: redemption_active
+
+        Returns 1 if redemption is active (i.e. production orders can be placed.)
+        Returns 0 if not.
+    */
+    function redemption_active()
+    {
+        $response = $this->_make_request("redemption_active");
+        return $response["redemption_active_response"]["redemption_active_result"];
+    }
+
+
+
+    /*
+        Function: list_available_catalogs
+        $list_available_catalogs_response = $api->list_available_catalogs();
+
+        $domain = $catalogs_ref["list_available_catalogs_response"]["list_available_catalogs_result"]["domain"];
+        print "The following sockets are available for the {$domain['account_name']} account.\n";
+
+        foreach($domain["sockets"]["Socket"] as $socket)
+        {
+            print "got socket: {$socket['socket_name']}\n";
+        }
+    */
+    function list_available_catalogs()
+    {
+        $response = $this->_make_request("list_available_catalogs");
+        $ref = $response["list_available_catalogs_response"]["list_available_catalogs_result"];
+        $this->_validate_response($ref["credentials"]);
+        return $ref;
+    }
+
+
+
+    /*
+        Function: catalog_breakdown
+
+        function process_category($category, $depth)
+        {
+            print str_repeat("\t", $depth) . "found category: {$category['name']} \n";
+
+            foreach($category["children"]["Category"] as $child)
+            {
+                process_category($child, $depth++);
+            }
+        }
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "is_flat" => 0
+        );
+
+        $breakdown = $api->catalog_breakdown($args);
+
+        foreach($breakdown["categories"]['Category'] as $category)
+        {
+            process_category($category, 0);
+        }
+
+    */
+    function catalog_breakdown($args)
+    {
+        $response = $this->_make_request("catalog_breakdown", $args);
+        $ref = $response["catalog_breakdown_response"]["catalog_breakdown_result"];
+        $this->_validate_response($ref["credentials"]);
+        return $ref;
+    }
+
+
+
+    /*
+        Function: search_catalog
+
+        Check the catalogapi.com documentation for a full list of search parameters.
+
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "search" => 'ipod'
+        );
+
+        $search_catalog_response = $api->search_catalog($args);
+
+        foreach($search_catalog_response["items"]['CatalogItem'] as $item)
+        {
+            print "found item: {$item['name']}\n";
+        }
+    */
+    function search_catalog($args)
+    {
+        $response = $this->_make_request("search_catalog", $args);
+        $ref = $response["search_catalog_response"]["search_catalog_result"];
+        $this->_validate_response($ref["credentials"]);
+        return $ref;
+    }
+
+
+
+    /*
+        Function: view_item
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "catalog_item_id" => $catalog_item_id
+        );
+
+        $view_item_response = $api->view_item($args);
+
+        $item = $view_item_response["item"];
+
+        print "found item: {$item['name']}\n{$item['description']}";
+    */
+    function view_item($args)
+    {
+        $response = $this->_make_request("view_item", $args);
+        $ref = $response["view_item_response"]["view_item_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        $item = &$ref["item"];
+        $tags = $item["tags"];
+        unset($item["tags"]);
+        $item["tags"] = $tags["string"] ? $tags["string"] : array();
+
+        $categories = $item["categories"];
+        unset($item["categories"]);
+        $item["categories"] = $categories["integer"] ? $categories["integer"] : array();
+
+        return $ref;
+    }
+
+    /*
+        Section: CART METHODS
+
+        Carts provide a way to build an order through a REST interface.
+        You can add and remove items, set a shipping address, and place an order.
+        These methods explain how.
+    */
+
+    /*
+        Function: cart_add_item
+
+        Adds items to the cart.
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "catalog_item_id" => $catalog_item_id,
+            "external_user_id" => 'johndoe123',
+            "option_id" => 123, # This is required for items that have options
+            "quantity" => 1
+        );
+
+        $cart_add_item_response = $api->cart_add_item($args);
+
+        print "{$cart_add_item_response['description']}\n";
+    */
+    function cart_add_item($args)
+    {
+        $response = $this->_make_request("cart_add_item", $args);
+        $ref = $response["cart_add_item_response"]["cart_add_item_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        return $ref;
+    }
+
+
+
+    /*
+        Function: cart_remove_item
+
+        Removes items from the cart.
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "catalog_item_id" => $catalog_item_id,
+            "external_user_id" => 'johndoe123',
+            "option_id" => 123 # This is required for items that have options
+        );
+
+        $cart_remove_item_response = $api->cart_remove_item($args);
+
+        print "{$cart_remove_item_response['description']}\n";
+    */
+    function cart_remove_item($args)
+    {
+        $response = $this->_make_request("cart_remove_item", $args);
+        $ref = $response["cart_remove_item_response"]["cart_remove_item_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        return $ref;
+    }
+
+
+
+    /*
+        Function: cart_set_item_quantity
+
+        Adds items to the cart. The quantity passed to this call overrides the quantity of a duplicate item.
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "catalog_item_id" => $catalog_item_id,
+            "external_user_id" => 'johndoe123',
+            "quantity" => 2,
+            "option_id" => 123 # This is required for items that have options
+        );
+
+        $cart_set_item_quantity_response = $api->cart_set_item_quantity($args);
+
+        print "{$cart_set_item_quantity_response['description']}\n";
+    */
+    function cart_set_item_quantity($args)
+    {
+        $response = $this->_make_request("cart_set_item_quantity", $args);
+        $ref = $response["cart_set_item_quantity_response"]["cart_set_item_quantity_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        return $ref;
+    }
+
+
+
+    /*
+        Function: cart_empty
+
+        Removes all items in the cart.
+
+        $args = array(
+            "socket_id" => $socket_id,
+            "external_user_id" => 'johndoe123'
+        );
+
+        $cart_empty_response = $api->cart_empty($args);
+
+        print "{$cart_empty_response['description']}\n";
+    */
+    function cart_empty($args)
+    {
+        $response = $this->_make_request("cart_empty", $args);
+        $ref = $response["cart_empty_response"]["cart_empty_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        return $ref;
+    }
+
+
+
+    /*
+        Function: cart_set_address
+
+        Adds a shipping address to the cart
+
+        $args = array(
+          socket_id => $socket_id,
+          external_user_id => 'johndoe123',
+          first_name => 'John',
+          last_name => 'Doe',
+          address_1 => '123 Test St.',
+          address_2 => 'Apt. B',
+          city => 'Cincinnati',
+          state_province => 'OH',
+          postal_code => '00000',
+          country => 'US',
+          email => 'johndoe123@example.com',
+          phone_number => '123-555-6789'
+        );
+
+        $cart_set_address_response = $api->cart_set_address($args);
+
+        print "{$cart_set_address_response['description']}\n";
+    */
+    function cart_set_address($args)
+    {
+        $response = $this->_make_request("cart_set_address", $args);
+        $ref = $response["cart_set_address_response"]["cart_set_address_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        return $ref;
+    }
+
+
+
+    /*
+        Function: cart_validate
+
+        Validates the address and items in the cart.
+        You should call this method just before placing an order to make sure that the order will not be rejected.
+
+        This method works a bit differently than the others.
+        It will return undef if the cart is valid,
+        otherwise it returns the error message string.
+
+        $args = array(
+          socket_id => $socket_id,
+          external_user_id => 'johndoe123',
+          locked =>0
+        );
+
+        $error = $api->cart_validate($args);
+
+        if ($error)
+        {
+            warn "the cart is invalid: $error";
+        }
+        else
+        {
+            print "the cart is valid\n";
+        }
+    */
+    function cart_validate($args)
+    {
+        $args["die_on_default"] = 0;
+        $response = $this->_make_request("cart_validate", $args);
+        $ref = $response["cart_set_address_response"]["cart_set_address_result"];
+        $this->_validate_response($ref["credentials"]);
+
+        return $ref;
+    }
+
+
+    function _make_request($method, $args = array())
     {
         $data = NULL;
         $this->error = NULL;
-        
+        $this->_reset_errors();
+
+        $die_on_fault;
+
+        if(isset($args[die_on_fault]))
+        {
+            unset($args[die_on_fault]);
+            $die_on_fault = TRUE;
+        }
+
+        $url = "https://" . $this->sub_domain . ($this->is_prod ? ".prod" : ".dev") . ".catalogapi.com/v1/rest/$method/?";
+        $url .= $this->_generate_checksum_args($method);
+        foreach ($args as $key => $value)
+        {
+            $url .= "&$key=" . rawurlencode($value);
+        }
+
+        // print("URL:" . $url . "\n");
+
+        $ch = curl_init( $url );
+
+        curl_setopt($ch,CURLOPT_TIMEOUT,30);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+        $result = curl_exec($ch);
+
         try
         {
-            $url = "https://" . $this->sub_domain . ($this->is_prod ? ".prod" : ".dev") . ".catalogapi.com/v1/" . $this->endpoint . "/$method/?";
-            $url .= $this->_generate_checksum_args($method);
-            foreach ($args as $key => $value)
-            {
-                $url .= "&$key=" . rawurlencode($value);
-            }
-            
-            #print("URL:" . $url . "\n");
-
-            // TODO reuse $ch?
-            $ch = curl_init( $url );
-            
-            curl_setopt($ch,CURLOPT_TIMEOUT,30);
-            curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-            
-            $result = curl_exec($ch);
-            curl_close($ch);
-            
-            if ($this->endpoint == "rest")
-            {
-                //print "REST!\n";
-                $data = json_decode($result, true); // true here returns arrays instead of objects
-            }
-            elseif ($this->endpoint == "restx")
-            {
-                //print "XML!\n";
-                // TODO return a DOM?
-                $data = $result;
-            }
+            $data = json_decode($result, true); // true here returns arrays instead of objects
         }
         catch (Exception $ex)
         {
-            $this->error = $ex->getMessage();
+            die("invalid response: " . $ex->getMessage());
         }
-        
-        return $data;
+
+        if(!curl_errno($ch) || !$die_on_fault)
+        {
+            curl_close($ch);
+            return $data;
+        }
+        else
+        {
+            if($data["Fault"]["detail"])
+            {
+                echo $data["Fault"]["detail"];
+            }
+
+            if($data["Fault"]["faultcode"])
+            {
+                $this->client_error = $data["Fault"]["faultstring"];
+            }
+            else
+            {
+                $this->server_error = $data["Fault"]["faultstring"];
+            }
+
+            die($data["Fault"]["faultcode"] . ": " . $data["Fault"]["faultstring"] . "\n");
+        }
+
     }
-    
+
+    function has_error()
+    {
+        return ($this->server_error || $this->client_error);
+    }
+
+    function _reset_errors()
+    {
+        $this->server_error = "";
+        $this->client_error = "";
+        return;
+    }
+
     function _generate_checksum_args($method)
     {
         $message_id = $this->_get_guid();
-        
+
         $now_datetime = new DateTime('NOW', new DateTimeZone('UTC'));
         $now_string = $now_datetime->format('Y-m-d H:i:s');
-        
+
         #print "NOW: $now_string\n";
-        
+
         $digest_string = "$method$message_id$now_string";
-        
+
         $checksum = base64_encode( hash_hmac("sha1", $digest_string, $this->secret_key, TRUE) );
-        
+
         return "creds_datetime=" . rawurlencode($now_string)
             . "&creds_uuid=" . rawurlencode($message_id)
             . "&creds_checksum=" . rawurlencode($checksum);
     }
-    
+
     function _get_guid()
     {
         if (function_exists('com_create_guid'))
@@ -119,6 +453,21 @@ class CatalogAPI
                 .substr($charid,20,12);
             return $uuid;
         }
+    }
+
+    function _validate_response($creds)
+    {
+
+        $digest_string = $creds["method"].$creds["uuid"].$creds["datetime"];
+        $their_checksum = $creds["checksum"];
+        $our_checksum = base64_encode( hash_hmac("sha1", $digest_string, $this->secret_key, TRUE) );
+
+        if($their_checksum != $our_checksum)
+        {
+            echo "the returned checksum is invalid";
+        }
+
+        return;
     }
 }
 
--- php/test.php
+++ php/test.php
@@ -1,39 +1,83 @@
 <?php
 require_once 'catalogapi.php';
 
-// usage: php test.php -d testco -k 'mysecretkey'
+// usage: php test.php -d testco -k 'mysecretkey' -s 'socket_id'
 
-$options = getopt("d:k:x");
+$options = getopt("d:k:s:");
 
-$catalogapi = new CatalogAPI($options["d"], $options["k"], ($options["x"] ? "restx" : "rest"), FALSE);
+$catalogapi = new CatalogAPI($options["d"], $options["k"]);
+$socket_id = $options["s"];
 
 $catalogs_ref = $catalogapi->list_available_catalogs();
+
 if ($catalogs_ref == NULL || $catalogs_ref == "")
 {
     print $catalogapi->error . "\n";
 }
 else
 {
-    if (!$options["x"])
-    {
-        print "DUMP: " . var_dump($catalogs_ref);
-        
-        // this is a BIT verbose ;)
-        print "Account: " . $catalogs_ref["list_available_catalogs_response"]["list_available_catalogs_result"]["domain"]["account_name"] . "\n";
-        
-        $socket_id = $catalogs_ref["list_available_catalogs_response"]["list_available_catalogs_result"]["domain"]["sockets"]["Socket"][0]["socket_id"] . "\n";
-        print "got socket_id: $socket_id\n";
-        
-        $breakdown = $catalogapi->catalog_breakdown($socket_id);
-        print var_dump($breakdown);
-        
-        $search = $catalogapi->search_catalog($socket_id, array( search => "ipod" ));
-        print var_dump($search);
-    }
-    else
-    {
-        print $catalogs_ref;
-    }
+    // print "DUMP: " . var_dump($catalogs_ref);
+
+    // // this is a BIT verbose ;)
+    // print "Account: " . $catalogs_ref["domain"]["account_name"] . "\n";
+
+    // $breakdown = $catalogapi->catalog_breakdown($socket_id);
+    // print var_dump($breakdown);
+
+    // $search = $catalogapi->search_catalog($socket_id, array( search => "" ));
+    // print var_dump($search);
+
+    // $catalog_item_id = $search["items"]["CatalogItem"][0]["catalog_item_id"] . "\n";
+    $catalog_item_id = 2386886;
+
+    // $view_item = $catalogapi->view_item($socket_id, $catalog_item_id);
+    // print var_dump($view_item);
+
+    $address_set = array(
+      socket_id => $socket_id,
+      external_user_id => 'johndoe123',
+      first_name => 'John',
+      last_name => 'Doe',
+      address_1 => '201 East Fourth Street',
+      address_2 => 'Suite 1850',
+      city => 'Cincinnati',
+      state_province => 'OH',
+      postal_code => '45202',
+      country => 'US',
+      email => 'johndoe123@example.com',
+      phone_number => '123-555-6789'
+    );
+
+    $add_item_args = array(
+        socket_id => $socket_id,
+        external_user_id => 'johndoe123',
+        catalog_item_id => $catalog_item_id,
+        quantity => 1,
+        option_id => NULL
+    );
+
+    $cart_set_address = $catalogapi->cart_set_address($address_set);
+    print "{$cart_set_address['description']}\n";
+
+    $cart_add_item = $catalogapi->cart_add_item($add_item_args);
+    print "{$cart_add_item['description']}\n";
+
+    $add_item_args[quantity] = 2;
+
+    $cart_set_item_quantity = $catalogapi->cart_set_item_quantity($add_item_args);
+    print "{$cart_set_item_quantity['description']}\n";
+
+    unset($add_item_args[quantity]);
+
+    $cart_remove_item = $catalogapi->cart_remove_item($add_item_args);
+    print "{$cart_remove_item['description']}\n";
+
+    unset($add_item_args[catalog_item_id]);
+
+    $cart_empty = $catalogapi->cart_empty($add_item_args);
+    print "{$cart_empty['description']}\n";
+
+
 }
 
 ?>
