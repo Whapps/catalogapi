23d016278fd774eea2551208569e51e6428187b8
Dennis Smith <dennis.smith@online-rewards.com>
2019-09-12 16:36:58 -0400

    Working on Test Script

--- php/test.php
+++ php/test.php
@@ -1,82 +1,65 @@
 <?php
+
 require_once 'catalogapi.php';
 
-// usage: php test.php -d testco -k 'mysecretkey' -s 'socket_id'
+// usage: php test.php -d testco -k 'mysecretkey'
 
-$options = getopt("d:k:s:");
+$options = getopt('d:k:');
 
-$catalogapi = new CatalogAPI($options["d"], $options["k"]);
-$socket_id = $options["s"];
+$api = new CatalogAPI($options['d'], $options['k'], FALSE);
 
-$catalogs_ref = $catalogapi->list_available_catalogs();
+$list_available_catalogs_response = $api->list_available_catalogs();
 
-if ($catalogs_ref == NULL || $catalogs_ref == "")
+$domain = $list_available_catalogs_response['domain'];
+
+print "The following sockets are available for the {$domain['account_name']} account.\n";
+
+$socket_id;
+foreach($domain['sockets']['Socket'] as $socket)
 {
-    print $catalogapi->error . "\n";
-}
-else
-{
-    // print "DUMP: " . var_dump($catalogs_ref);
-
-    // // this is a BIT verbose ;)
-    // print "Account: " . $catalogs_ref["domain"]["account_name"] . "\n";
-
-    // $breakdown = $catalogapi->catalog_breakdown($socket_id);
-    // print var_dump($breakdown);
-
-    // $search = $catalogapi->search_catalog($socket_id, array( search => "" ));
-    // print var_dump($search);
-
-    // $catalog_item_id = $search["items"]["CatalogItem"][0]["catalog_item_id"] . "\n";
-    $catalog_item_id = 2386886;
-    // $order_number = '9658-02828-19455-0001';
-
-    // $view_item = $catalogapi->view_item(array(socket_id => $socket_id, catalog_item_id => $catalog_item_id));
-    // print var_dump($view_item);
-
-    $add_item_args = array(
-        catalog_item_id => $catalog_item_id,
-        quantity => 1,
-        currency => "USD",
-        catalog_price => 25.00,
-        option_id => NULL
-    );
-
-    $args = array(
-        socket_id => $socket_id,
-        external_user_id => 'johndoe123',
-        first_name => 'John',
-        last_name => 'Doe',
-        address_1 => '201 East Fourth Street',
-        address_2 => 'Suite 1850',
-        city => 'Cincinnati',
-        state_province => 'OH',
-        postal_code => '45202',
-        country => 'US',
-        email => 'johndoe123@example.com',
-        phone_number => '123-555-6789',
-        items => array(
-            $add_item_args
-        )
-    );
-
-
-    $order_number = $catalogapi->order_place($args);
-    print "the order was created with the order_number: $order_number\n";
-
-
-
-    // unset($add_item_args[quantity]);
-    //
-    // $cart_remove_item = $catalogapi->cart_remove_item($add_item_args);
-    // print "{$cart_remove_item['description']}\n";
-    //
-    // unset($add_item_args[catalog_item_id]);
-    //
-    // $cart_empty = $catalogapi->cart_empty($add_item_args);
-    // print "{$cart_empty['description']}\n";
-
-
+    print "\t{$socket['socket_name']} (socket_id: {$socket['socket_id']})\n";
+    $socket_id = $socket['socket_id'];
 }
 
-?>
+print "\nRetreiving categories for socket_id $socket_id...\n";
+
+$args = array(
+    socket_id => $socket_id,
+    is_flat => 0
+);
+
+$catalog_breakdown_response = $api->catalog_breakdown($args);
+
+function process_category($category, $depth)
+{
+    print str_repeat("\t", $depth) . "found category: {$category['name']} \n";
+
+    foreach($category["children"]["Category"] as $child)
+    {
+        process_category($child, $depth++);
+    }
+}
+
+foreach($catalog_breakdown_response["categories"]['Category'] as $category)
+{
+    process_category($category, 0);
+}
+
+print "\nSearching the catalog for the word \"card\"...\n";
+
+$args = array(
+    socket_id => $socket_id,
+    search => 'card'
+);
+
+$search_catalog_response = $api->search_catalog($args);
+
+$catalog_item;
+foreach($search_catalog_response['items']['CatalogItem'] as $item)
+{
+    print "\tFound match: {$item['name']}\n";
+    $catalog_item = $item;
+}
+
+
+print "\nRetrieving the full description for catalog_item_id {$catalog_item['catalog_item_id']}\n";
